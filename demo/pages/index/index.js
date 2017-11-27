ea0e34e993eba552aa1a3addd3526eeba6f0c0a2
zhuRon <940223872@qq.com>
2017-11-27 21:56:12 +0800

    11.27

--- demo/pages/index/index.js
+++ demo/pages/index/index.js
@@ -4,57 +4,68 @@ const app = getApp()
 
 Page({
   data: {
-    activeIndex: 0,
-    jobs: []
+    activeIndex:0,
+    jobs:[{}]
+    // nursery_list:[{}],
+    // earlyTeach_list:[{}]
   },
-
-  changTab:  function(e){
-    var type =
-     e.target.dataset.index;
+  changTab: function(e){
+    var type = e.target.dataset.index;
     this.setData({
       activeIndex: type
     });
     this.get_job_list(type);
   },
   swiperTab: function(e){
-    var type = 
-    e.detail.current;
+    var type = e.detail.current;
     this.setData({
-      activeIndex: type
+      activeIndex:type
     });
     this.get_job_list(type);
   },
   //事件处理函数
   bindViewTap: function() {
-    
+    // wx.navigateTo({
+    //   url: '../logs/logs'
+    // })
   },
   // 获取工作岗位列表
   get_job_list: function(type){
     var that = this;
     wx.request({
-      url: 'http://jxufer.cn:3000/api/position',
-      method: 'GET',
-      data: {
-        type: type
+      url:'http://jxufer.cn:3000/api/position',
+      method:'GET',
+      data:{
+        type:type
       },
       success: function(res){
+       //  console.log(res);
+        var data = res.data;
         if(type==1){
           that.setData({
-          nursery_list: res
+          nursery_list: res.data
         });
         }else if(type==2){
           that.setData({
-          earlyTeach_list: res
+          earlyTeach_list: res.data
         });
         }else{
           that.setData({
-          jobs:  res.data.data
+          jobs:res.data.data
         });
         }
       }
     })
   },
   onLoad: function () {
-    this.get_job_list();
+    this.get_job_list("");
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   }
 })
